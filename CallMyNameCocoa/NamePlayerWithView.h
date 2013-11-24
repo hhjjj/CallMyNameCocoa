ac8e16f657e90e1baa5ac9b2aa4cb97e2488c485
hojun <songhojun@gmail.com>
2013-11-24 21:18:51 +0900

    files is loaded but memory is adding

---
+++ CallMyNameCocoa/NamePlayerWithView.h
@@ -0,0 +1,13 @@
+//
+//  NamePlayerWithView.h
+//  CallMyNameCocoa
+//
+//  Created by hojun on 11/24/13.
+//  Copyright (c) 2013 hojun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NamePlayerWithView : NSView
+
+@end
---
+++ CallMyNameCocoa/NamePlayerWithView.m
@@ -0,0 +1,29 @@
+//
+//  NamePlayerWithView.m
+//  CallMyNameCocoa
+//
+//  Created by hojun on 11/24/13.
+//  Copyright (c) 2013 hojun. All rights reserved.
+//
+
+#import "NamePlayerWithView.h"
+
+@implementation NamePlayerWithView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
